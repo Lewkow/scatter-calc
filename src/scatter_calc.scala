8c200f22144385ab060b4eea0e19024795d48a6d
Nicholas Lewkow <nicholas.lewkow@mheducation.com>
2015-03-01 11:20:22 -0500

    changed object type for main function, added some tests which init Scatter object and print some basic values

--- src/scatter_calc.scala
+++ src/scatter_calc.scala
@@ -2,17 +2,13 @@ package runtime
 
 import scatter.Scatter
 
-object Main extends App{
-  val test_scatter = new scatter.Scatter(10.0d, "H", "H")
-  test_scatter.printValues()
+object scatter_calc {
+
+  def main(args: Array[String]) {
+    val test_scatter = new Scatter(10.0d, "H", "H")
+    test_scatter.printValues()
+    test_scatter.setup_phaseshifts()
+  }
+
 }
-
-// object scatter_calc {
-
-//   def main(args: Array[String]) {
-//     val test_scatter = Scatter(10.0d, "H", "H")
-//     test_scatter.printValues()
-//   }
-
-// }
 

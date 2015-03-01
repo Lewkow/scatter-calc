83de0326d2b42cd96e510e7a344299a9e1682a71
Nicholas Lewkow <nicholas.lewkow@mheducation.com>
2015-03-01 11:22:22 -0500

    added Potential class

---
+++ src/potential.scala
@@ -0,0 +1,24 @@
+package potential
+
+class Potential(potential_methodC: String) {
+  val potential_method = potential_methodC
+
+  // pass interparticle distance [u]
+  def get_potential(r: Double) = {
+    if (potential_method == "LJ") {
+      lennard_jones(r, 1.0d, 1.0d)
+    }
+  }
+
+  def lennard_jones(r: Double, 
+                    well_depth: Double,
+                    wall_pos: Double) = {
+    if (r <= 0) {
+      0.0d
+    }
+    else {
+      val x = wall_pos/r
+      4.0d*well_depth*(math.pow(x,12.0)-math.pow(x,6.0))
+    }
+  }
+}

89f6c3870ace50209414657d88beea4bc567b706
Nicholas Lewkow <nicholas.lewkow@mheducation.com>
2015-03-04 20:31:14 -0500

    pass potential type to init Scatter object

--- src/scatter_calc.scala
+++ src/scatter_calc.scala
@@ -5,9 +5,8 @@ import scatter.Scatter
 object scatter_calc {
 
   def main(args: Array[String]) {
-    val test_scatter = new Scatter(10.0d, "H", "H")
+    val test_scatter = new Scatter(10.0d, "H", "H", "LJ")
     test_scatter.printValues()
-    test_scatter.setup_phaseshifts()
   }
 
 }

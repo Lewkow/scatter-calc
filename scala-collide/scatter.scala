257d4b51dd90a48e177a3336248b4943668e8b0c
Nicholas Lewkow <nicholas.lewkow@mheducation.com>
2015-03-19 18:59:37 -0400

    implemented basic grid class

--- scala-collide/scatter.scala
+++ scala-collide/scatter.scala
@@ -1,6 +1,8 @@
 
 package scatter
 
+import potential.Potential
+
 class Scatter(energyC: Double, 
               projectileC: String, 
               targetC: String,
@@ -19,7 +21,7 @@ class Scatter(energyC: Double,
   val target_mass: Double = masses(target)
   val reduced_mass: Double = reducedMass(projectile_mass,target_mass)
   val wavenumber: Double = math.sqrt(2.0d*reduced_mass*energy)
-  val phase = new Phaseshift(potential_type)
+  val phase = new Phaseshift(energy, potential_type)
 
 
 
@@ -30,44 +32,62 @@ class Scatter(energyC: Double,
   }
 
   def printValues() {
-    println(s"Projectile: $projectile Mass: $projectile_mass [u]")
-    println(s"Target: $target Mass: $target_mass [u]")
-    println(s"Energy: $energy [eV]")
+    val grid_start = phase.grid.start_pos 
+    val grid_end = phase.grid.end_pos 
+    println(s"Projectile:   $projectile") 
+    println(s"Mass:         $projectile_mass [u]")
+    println(s"Target:       $target Mass: $target_mass [u]")
+    println(s"Energy:       $energy [eV]")
     println(s"Reduced Mass: $reduced_mass [u]")
+    println(s"Grid Start:   $grid_start [a0]")
+    println(s"Grid End:     $grid_end [a0]")
   }
 
 }
 
-class Phaseshift(potential_type: String) {
+class Phaseshift(energy: Double, potential_type: String) {
   val MAX_GRID_SIZE = 100000
-  // var grid_step_size: Double = 0.0d
-  var grid_start: Double = 0.001d
-  var grid_end: Double = 10.0d
-  val grid = new Grid(grid_start, 
-                  grid_end, 
-                  10,
-                  // MAX_GRID_SIZE,
-                  potential_type)
-  // val pos_grid = grid.build_pos_grid()
-  // val pot_grid = grid.build_pot_grid(potential_type)
-
-  def grid_start_finder(energy: Double) = {
-    0.0d
-  }
-
+  val pot = new Potential(potential_type)
+  val grid = new Grid(energy, pot)
 }
 
-class Grid(startC: Double, 
-           endC: Double, 
-           N_gridC: Int,
-           potential_typeC: String) {
-  val start_pos: Double = startC
-  val end_pos: Double = endC
-  val N_grid: Int = N_gridC
+class Grid(energyC: Double, pot: Potential) {
+  val energy: Double = energyC
+  val start_pos: Double = grid_start_finder() 
+  val end_pos: Double = grid_end_finder()
+  val N_grid: Int = get_N_grid() 
   val dx: Double = (end_pos - start_pos)/(N_grid.toDouble - 1.0d)
-  val potential_type: String = potential_typeC
   val pos_grid = build_pos_grid()
   val pot_grid = build_pot_grid()
+
+  def get_N_grid(): Int = {
+    10000
+  }
+
+  def grid_end_finder() = {
+    10.0d
+  }
+
+  def grid_start_finder() = {
+    var i: Int = 0
+    var j: Int = 0 
+    val start: Double = 10.0d
+    val dr: Double = 0.0001
+    val lim: Double = energy*1.3d
+    var going: Boolean = true
+    var r: Double = start
+
+    while (going == true) {
+      r = start - j.toDouble*dr
+      var pot_r: Double = pot.get_potential(r)
+      if ( (pot_r >= lim) | (r <= 1.0e-5) ) {
+        going = false 
+      } else {
+        j = j + 1
+      }
+    }
+    r
+  }
 
   def build_pos_grid() = {
     val z = new Array[Double](N_grid)    
@@ -79,8 +99,6 @@ class Grid(startC: Double,
   }
 
   def build_pot_grid() = {
-    import potential.Potential
-    val pot = new Potential(potential_type)
     val z = new Array[Double](N_grid)
     var i: Int = 0
     for (i <- 0 to z.length-1) {

fb15516ff934d198881623b816302ddac027ebf9
Nicholas Lewkow <nicholas.lewkow@mheducation.com>
2015-03-20 10:41:03 -0400

    changed default test energy to 100 eV

--- scala-collide/scatter_calc.scala
+++ scala-collide/scatter_calc.scala
@@ -5,7 +5,7 @@ import scatter.Scatter
 object scatter_calc {
 
   def main(args: Array[String]) {
-    val test_scatter = new Scatter(10.0d, "H", "H", "LJ")
+    val test_scatter = new Scatter(100.0d, "H", "H", "LJ")
     test_scatter.printValues()
   }
 
